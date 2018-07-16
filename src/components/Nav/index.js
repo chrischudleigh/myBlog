7ac9c4895b8e4fa1d6f9fc7e0f3b9a1721614256
Chris Chudleigh <chrischudleigh@chris.local>
2018-07-16 12:17:44 +1000

    updated homepage link

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,21 +5,15 @@ const myNav= () => (
   <Navbar collapseOnSelect>
   <Navbar.Header>
     <Navbar.Brand>
-    <Link to="/index/">Web Bytes</Link>
+    <Link to="/">Web Bytes</Link>
     </Navbar.Brand>
     <Navbar.Toggle />
   </Navbar.Header>
   <Navbar.Collapse>
     <Nav>
-      <NavItem eventKey={1}>
-      <Link to="/page-2/">About</Link>
-      </NavItem>
-      <NavItem eventKey={2}>
-      <Link to="/page-3/">Contact-Us</Link>
-      </NavItem>
-     
+      <NavItem eventKey={1}><Link to="/page-2/">About</Link></NavItem>
+      <NavItem eventKey={2}><Link to="/page-3/">Contact-Us</Link></NavItem>
     </Nav>
-    
   </Navbar.Collapse>
 </Navbar>
 
