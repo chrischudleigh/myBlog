6a9fad73692f45614e98db1df70ec878cf4a949e
Chris Chudleigh <chrischudleigh@chris.local>
2019-04-15 11:20:05 +1000

    minor content changes

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,8 +3,10 @@ import Link from 'gatsby-link'
 
 const SecondPage = () => (
   <div>
-    <h1>About Us</h1>
-   
+    <h1>About</h1>
+    
+    <p>I am web developer/designer from Brisbane, Australia.</p>
+
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -5,6 +5,7 @@ const SecondPage = () => (
   <div>
     <h1>Contact Us</h1>
    
+   <p>Email: <a href="mailto:chris.chudleigh@gmail.com">chris.chudleigh@gmail.com</a></p>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
